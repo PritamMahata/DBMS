6e692a604c742a1d214605b2802996aabd83c711
Pritam <pritammahata12@gmail.com>
2024-01-26 17:24:49 +0530

    modify scripts

---
+++ SQL_scripts.sql
@@ -0,0 +1,11 @@
+use college;
+create database oracleDB;
+drop database oracleDB;
+show databases;
+create table dept(
+  id int,
+  name varchar(50),
+  age int not null
+);
+# select * from dept;
+# drop table dept;
