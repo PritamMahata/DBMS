03adfb1970f2d2c171c90296d0b2b496292d77bb
Pritam <pritammahata12@gmail.com>
2024-03-24 19:19:09 +0530

    MODIFY QUERY

--- LAB Manual Assignment/assignment-2.sql
+++ LAB Manual Assignment/assignment-2.sql
@@ -10,7 +10,7 @@ create table employee2
     Salary    decimal
 );
 
---  insert 5 row in the table
+-- 1. Insert 5 row in the table
 insert into employee2
 values (11, 'RAVI', 'KOLKATA', '9845353223', 10, 'SALES', 222, 23000);
 insert into employee2
@@ -22,27 +22,27 @@ values (14, 'SUMIT', 'KOLKATA', '9162342401', 10, 'SALES', 555, 24000);
 insert into employee2
 values (15, 'HARIPRASHAD', 'HOWRAH', '9862342401', 20, 'SALES', 666, 25000);
 
--- display all record from Employee table
+--2. Display all record from Employee table
 select *
 from employee2;
 
--- display the record of each employee who works in department D10
+-- 3. Display the record of each employee who works in department D10
 select E_NAME
 from employee
 where Dept_no = 10;
 
--- update the city of emp_no-12 with works in department as Nagpur
+-- 4. Update the city of Emp_no-12 with works in department as Nagpur
 UPDATE employee
 SET E_address = 'NAGPUR'
 where Dept_no = 12;
 
 
--- display the details of employee who works in department MECH
+-- 5. Display the details of employee who works in department MECH
 select *
 from employee
 where Dept_Name = 'MECH';
 
--- display the record who are work in sales department
+-- 6. Display the complete record of employees working in SALES Department.
 select E_name
 from employee
 where Dept_Name = 'SALES';
